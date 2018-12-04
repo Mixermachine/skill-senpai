2c962219025f1cbc25d1c1fab9ab7474a795a9bd
Stephan <ga58max@mytum.de>
2018-12-04 22:36:50 +0100

    create base database structure

--- studysite/skill_senpai/admin.py
+++ studysite/skill_senpai/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from skill_senpai import models
 
 # Register your models here.
+
+admin.site.register(models.Lecture)
+admin.site.register(models.Category)
+admin.site.register(models.Language)
--- studysite/skill_senpai/models.py
+++ studysite/skill_senpai/models.py
@@ -3,6 +3,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Subject(models.Model):
-    description = models.TextField()
-    pub_date = models.DateTimeField('date published')
+class Category(models.Model):
+	name = models.CharField(max_length=255)
+
+
+class Lecture(models.Model):
+
+	description = models.TextField()
+	lecture_id = models.CharField(max_length=255, null=True)
+   	# preconditions ManyToMany
+	categorie = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+	credits = models.IntegerField(null=True)
+	time = models.TimeField(null=True)
+	nevents = models.IntegerField(null=True)
+	tutorial = models.BooleanField(default=False)
+   	#languages = models.Many
+
+	pub_date = models.DateTimeField('date published')
+
+
+# Categorie Model
+
+
+
+class Language(models.Model):
+	name = models.CharField(max_length=255)
