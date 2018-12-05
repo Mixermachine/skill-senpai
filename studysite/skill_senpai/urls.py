68b08db66f8eb872744775db568ba7b6a62142f4
Stephan <ga58max@mytum.de>
2018-12-05 01:52:34 +0100

    add query_lectures view

--- studysite/skill_senpai/urls.py
+++ studysite/skill_senpai/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from skill_senpai.views import IndexView
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/$', IndexView.as_view()),
+    url(r'^about/$', views.IndexView.as_view()),
+    url(r'^query_lectures/$', views.query_lectures, name='query_lectures'),
 ]
--- studysite/skill_senpai/views.py
+++ studysite/skill_senpai/views.py
@@ -5,9 +5,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+import json
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the skill_senpais index.")
+
+
+def query_lectures(request):
+
+	# json object to return
+	data
+
+	if request.method=='POST':
+
+		received_json_data=json.loads(request.POST['data'])
+		# fill json response here
+		data = {'foo': 'bar', 'hello': 'world'}
+
+	return HttpResponse(json.dumps(data), content_type='application/json')
+
+
 
 class IndexView(TemplateView):
     template_name = "about.html"
