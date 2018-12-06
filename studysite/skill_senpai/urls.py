4db4c7ba337e558b2100b03a57c27ca1bdd64fdd
Nico Schreder <nico.schreder@tum.de>
2018-12-06 18:10:23 +0100

    Change structure of site

--- studysite/skill_senpai/urls.py
+++ studysite/skill_senpai/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.IndexView.as_view()),
+    url(r'^index/$', views.IndexView.as_view()),
     url(r'^api/query_lectures/$', views.query_lectures, name='query_lectures'),
 ]
