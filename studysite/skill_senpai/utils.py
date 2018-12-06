de89c3b505d2acceee476671e371fabff070a35b
Mixermachine <contact@9d3.de>
2018-12-06 18:25:51 +0100

    Added database query to get_lecture

--- studysite/skill_senpai/utils.py
+++ studysite/skill_senpai/utils.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from skill_senpai.models import Lecture
 
 
 def get_lecture(skills):
+	Lecture.objects.none()
+
+	return_list = []
+
 	# nicu desu
+	for item in skills:
+		return_list.append(Lecture.objects.get(lecture_id=item))
 
-
-
-	pass
+	return return_list
 
 
 def get_preconditions():
