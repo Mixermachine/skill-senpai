be1a6940aa144a347312638b7e1afbb06d6412f9
Stephan <ga58max@mytum.de>
2018-12-06 18:11:20 +0100

    update models

--- studysite/skill_senpai/models.py
+++ studysite/skill_senpai/models.py
@@ -14,18 +14,16 @@ class Language(models.Model):
 
 class Lecture(models.Model):
 
-	description = models.TextField()
+	description = models.TextField(null=True)
 	lecture_id = models.CharField(max_length=255, null=True)
-   	preconditions = models.ManyToManyField("self")
+   	preconditions = models.ManyToManyField("self", blank=True)
 	categorie = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 	credits = models.IntegerField(null=True)
 	time = models.TimeField(null=True)
 	nevents = models.IntegerField(null=True)
 	tutorial = models.BooleanField(default=False)
-   	lanagues = models.ForeignKey(Language, on_delete=models.CASCADE, null=True)
+   	languages = models.ForeignKey(Language, on_delete=models.CASCADE, null=True)
 	pub_date = models.DateTimeField('date published')
 
-
-# Categorie Model
 
 
