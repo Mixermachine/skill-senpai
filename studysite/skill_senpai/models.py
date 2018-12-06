2387f0bdb814e2ee10e5f1d259ad3d2782a9dfb3
Stephan <ga58max@mytum.de>
2018-12-06 17:03:08 +0100

    update lecture model

--- studysite/skill_senpai/models.py
+++ studysite/skill_senpai/models.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Category(models.Model):
+	name = models.CharField(max_length=255)
+
+
+class Language(models.Model):
 	name = models.CharField(max_length=255)
 
 
@@ -11,20 +16,16 @@ class Lecture(models.Model):
 
 	description = models.TextField()
 	lecture_id = models.CharField(max_length=255, null=True)
-   	# preconditions ManyToMany
+   	preconditions = models.ManyToManyField("self")
 	categorie = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 	credits = models.IntegerField(null=True)
 	time = models.TimeField(null=True)
 	nevents = models.IntegerField(null=True)
 	tutorial = models.BooleanField(default=False)
-   	#languages = models.Many
-
+   	lanagues = models.ForeignKey(Language, on_delete=models.CASCADE, null=True)
 	pub_date = models.DateTimeField('date published')
 
 
 # Categorie Model
 
 
-
-class Language(models.Model):
-	name = models.CharField(max_length=255)
