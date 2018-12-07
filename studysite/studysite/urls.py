70a91c48ce68dadb86ee9eaf84634e1ac948bbe9
Mixermachine <contact@9d3.de>
2018-12-07 16:19:04 +0100

    Automatically redirect to senpai

--- studysite/studysite/urls.py
+++ studysite/studysite/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/skill_senpai')),
     url(r'^skill_senpai/', include('skill_senpai.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
