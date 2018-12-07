c9a3b35e09df25666021435a5b5b4481a51f0f38
Stephan <ga58max@mytum.de>
2018-12-07 11:16:55 +0100

    add DetailView for Lecture

--- studysite/skill_senpai/urls.py
+++ studysite/skill_senpai/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/query_lectures/$', views.query_lectures, name='query_lectures'),
+    url(r'^lecture/(?P<pk>\d+)/$', views.LectureDetailView.as_view(), name='lecture-detail'),
 ]
--- studysite/skill_senpai/views.py
+++ studysite/skill_senpai/views.py
@@ -4,8 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 from django.template import Context, loader
+from django.utils import timezone
 from utils import get_lecture
+
+from models import Lecture
 
 import json
 
@@ -22,3 +26,16 @@ def query_lectures(request):
 		return JsonResponse(get_lecture(received_json_data))
 
 	return HttpResponse(status=404)
+
+class LectureDetailView(DetailView):
+
+	model = Lecture
+	template_name = 'lecture_detail.html'
+
+	def get_context_data(self, **kwargs):
+
+		context = super(LectureDetailView, self).get_context_data(**kwargs)
+		context['now'] = timezone.now()
+
+
+		return context
