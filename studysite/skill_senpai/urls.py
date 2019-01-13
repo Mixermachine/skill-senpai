44e1551915c8f2a62c5e3959cfd7aabe2393ea29
Mixermachine <contact@9d3.de>
2019-01-13 19:38:56 +0100

    New_front_end: - Moved basic html skeleton into template/base/base.html. - Adapted texts and pictures to fit to Skill Senpai. - Moved base url from /skill-senpai to / - Optimized views processor - Added minimized version of some scripts

--- studysite/skill_senpai/urls.py
+++ studysite/skill_senpai/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.simpleProcessor, name='index'),
+    url(r'^pre-lectures/$', views.simpleProcessor, name='pre-lectures'),
+    url(r'^curriculum/$', views.simpleProcessor, name='curriculum'),
+    url(r'contact/', views.simpleProcessor, name='contact'),
     url(r'^api/query_lectures/$', views.query_lectures, name='query_lectures'),
     url(r'^lecture/(?P<lecture_id>\w+)/$', views.LectureDetailView.as_view(), name='lecture-detail'),
 ]
